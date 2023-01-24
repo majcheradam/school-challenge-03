e2ad37afe2988c2f77515562e23db754c34da8eb
Domun <zwolinskid1@gmail.com>
2023-01-24 13:15:45 +0100

    Update 3
    - fixed small bugs
    - added 2 new icon
    - added "main" & "content"

--- app/page.jsx
+++ app/page.jsx
@@ -8,5 +8,31 @@ const roboto = Roboto({
 })
 
 export default function Home() {
-  return <div className={roboto.className}></div>
+  return (
+    <div className={roboto.className}>
+      <main>
+        <div className="main-conteiner">
+          <div className="content">
+            <Image
+              src="/OpenAI_icon.png"
+              alt="Logo"
+              width="984"
+              height="273"
+              className="logo"
+            />
+            <div className="content-text-box">
+              <span>
+                <h1>Imagine if the best AI models were open and free.</h1>
+              </span>
+              <h1>What would you do with them?</h1>
+              <h2>
+                Open AI is working to make the world's best AI models open.
+              </h2>
+            </div>
+          </div>
+          <div className="content-secondary"></div>
+        </div>
+      </main>
+    </div>
+  )
 }
